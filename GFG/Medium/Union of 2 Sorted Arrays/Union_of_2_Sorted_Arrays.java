b6a8a64ace174624dd803ea632447c779d8aae26
Ayan Bhattacharjee <bhattacharjeeayan93@gmail.com>
2025-09-03 04:13:51 +0530

    Add solution: Union of 2 Sorted Arrays (GfgSoln)

---
+++ GFG/Medium/Union of 2 Sorted Arrays/Union_of_2_Sorted_Arrays.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        int i = 0, j = 0;
+        int n = a.length, m = b.length;
+        ArrayList<Integer> res = new ArrayList<>();
+
+        while (i < n && j < m) {
+            if (a[i] < b[j]) {
+                addIfNotDuplicate(res, a[i]);
+                i++;
+            } else if (b[j] < a[i]) {
+                addIfNotDuplicate(res, b[j]);
+                j++;
+            } else { // both equal
+                addIfNotDuplicate(res, a[i]);
+                i++;
+                j++;
+            }
+        }
+
+        while (i < n) {
+            addIfNotDuplicate(res, a[i]);
+            i++;
+        }
+
+        while (j < m) {
+            addIfNotDuplicate(res, b[j]);
+            j++;
+        }
+
+        return res;
+    }
+
+    private static void addIfNotDuplicate(ArrayList<Integer> res, int val) {
+        if (res.isEmpty() || res.get(res.size() - 1) != val) {
+            res.add(val);
+        }
+    }
+}
