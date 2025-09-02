5b18ed37421f9f247bf8767fe97b90d7976700d2
Ayan Bhattacharjee <bhattacharjeeayan93@gmail.com>
2025-09-03 04:26:24 +0530

    Update Intersection_of_Two_Arrays.java

--- LeetCode/Easy/Intersection of Two Arrays/Intersection_of_Two_Arrays.java
+++ LeetCode/Easy/Intersection of Two Arrays/Intersection_of_Two_Arrays.java
@@ -10,9 +10,9 @@ class Solution {
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) i++;  //nums1[i] cannot have a partner
             else if (nums2[j] < nums1[i]) j++; //nums2[j] cannot have a partner
-            else {   //ya toh jab pehla list khali hai tb ya phir direct entry(only after checking the entry is unique or not)
-                if (list.isEmpty() || list.get(list.size() - 1) != nums1[i]) 
-                    list.add(nums1[i]);
+            else {      //abhi bass nums1[i]==nums2[j] option hi bacha.....
+                if (list.isEmpty() || list.get(list.size() - 1) != nums1[i])    //ya toh jab pehla list khali hai tb 
+                    list.add(nums1[i]);    //ya phir direct entry(only after checking the entry is unique or not)
                 i++; j++;
             }
         }
